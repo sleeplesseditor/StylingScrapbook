c04104bb42a8f0f920cb0e7b22ef19b46b6b0dbf
Josh Bryson <josh@bryson.me.uk>
2020-03-22 17:51:36 +0000

    More button examples added

--- src/pages/ButtonPage/ButtonPage.jsx
+++ src/pages/ButtonPage/ButtonPage.jsx
@@ -9,32 +9,49 @@ function ButtonPage() {
         <h2>Button Page</h2>
         <div className="button-container">
           <div className="button-container-frame">
-            <a href="" className="button-hover-1">Hover Me</a>
+            <button className="button-hover-1">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-2">Hover Me</a>
+            <button className="button-hover-2">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-3">Hover Me</a>
+            <button className="button-hover-3">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-4">Hover Me</a>
+            <button className="button-hover-4">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-5">Hover Me</a>
+            <button className="button-hover-5">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-6">Hover Me</a>
+            <button className="button-hover-6">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-7">Hover Me</a>
+            <button className="button-hover-7">Hover Me</button>
           </div>
           <div className="button-container-frame">
-            <a href="" className="button-hover-8">Hover Me</a>
+            <button className="button-hover-8">Hover Me</button>
           </div>
         </div>
         <div className="navbar-container">
           <CascadingNavbar />
+        </div>
+        <div className="button-container">
+          <div className="button-container-frame">
+            <button className="button-hover-9">Hover Me</button>
+          </div>
+          <div className="button-container-frame">
+            <button className="button-hover-10">Hover Me</button>
+          </div>
+          <div className="button-container-frame">
+            <button className="button-hover-11">Hover Me</button>
+          </div>
+          <div className="button-container-frame">
+            <button className="button-hover-12"><span></span>Hover Me</button>
+          </div>
+          <div className="button-container-frame">
+            <button className="button-hover-13">Hover Me</button>
+          </div>
         </div>
     </div>
   );
