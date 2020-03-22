392852e0f96817cc0f534347008a1a3f7472d814
Josh Bryson <josh@bryson.me.uk>
2020-03-22 18:57:11 +0000

    Initial examples of image hover effects added

--- src/pages/ImageEffects/ImageEffects.jsx
+++ src/pages/ImageEffects/ImageEffects.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Image1 from '../../img/image1.png';
 import './ImageEffects.scss';
 
 function ImageEffects() {
   return (
     <div className="App">
         <h2>Image Effects Page</h2>
+        <div className="gallery-container">
+          <div className="image-container-1">
+            <img className="image-container-1-img" src={Image1} alt=""/>
+            <div className="image-container-1-caption">
+              <h1>amazing caption</h1>
+              <p>some generic content text</p>
+            </div>
+          </div>
+          <div className="image-container-2">
+            <img className="image-container-2-img" src={Image1} alt=""/>
+            <div className="image-container-2-caption">
+              <h1>amazing caption</h1>
+              <p>some generic content text</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
