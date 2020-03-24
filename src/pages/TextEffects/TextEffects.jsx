1c6bc6ca92fc8c0525a14441133d20601336bb10
Josh Bryson <josh@bryson.me.uk>
2020-03-24 00:02:24 +0000

    Smoky text effect added

--- src/pages/TextEffects/TextEffects.jsx
+++ src/pages/TextEffects/TextEffects.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import './TextEffects.scss';
 
 function TextEffects() {
   return (
     <div className="App">
         <h2>Text Effects Page</h2>
+        <div className="text-container">
+          <div className="text-container-content-1">
+            <ul>
+              <li>s</li>
+              <li>m</li>
+              <li>o</li>
+              <li>k</li>
+              <li>y</li>
+            </ul>
+          </div>
+        </div>
     </div>
   );
 }
