96e37a628d06a398d14ff91a90c2ba43f0601b75
Josh Bryson <josh@bryson.me.uk>
2020-04-10 00:44:01 +0100

    Light switch added to dark mode page

--- src/pages/DarkModePage/DarkModePage.jsx
+++ src/pages/DarkModePage/DarkModePage.jsx
@@ -3,23 +3,42 @@ import React, { useState } from 'react';
 import './DarkModePage.scss';
 
 function DarkModePage() {
-  const [switched, setSwitched] = useState(true);
+  const [switched, setSwitched] = useState(false);
   const toggleDarkMode = () => {
     setSwitched(!switched)
   };
 
+  const renderDarkModeArea = () => {
+    return (
+      <div className="dark-mode-feature-1">
+        <button className="button">
+          Neon Button
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="App" id={switched ? "light-mode-page" : "dark-mode-page"}>
-        <h2>Dark Mode Page </h2>
-        <div className="button-container">
-          <button
-            className="dark-mode-button"
-            onClick={() => {toggleDarkMode()}}
-          >
-            {switched ? 'Light Mode' : 'Dark Mode'}
-          </button >
+      <h2>Dark Mode Page </h2>
+      <div className="button-container">
+        <input
+          id="dark-mode-button"
+          type="checkbox"
+          onClick={() => {toggleDarkMode()}}
+        />
+        <label htmlFor="dark-mode-button" id="light-switch-label">
+          <div></div>
+          <div className="switch"></div>
+          <div></div>
+        </label>
+      </div>
+        <div className="dark-mode-container">
+          {
+            switched ? renderDarkModeArea() : null
+          }
         </div>
-    </div>
+      </div>
   );
 }
 
